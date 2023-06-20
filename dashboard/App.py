5119110a8cb0d289fab82f0343bbb944052eb213
Alex LI <zhi.li@datapebbles.com>
2023-06-20 17:40:46 +0000

    updates

--- dashboard/App.py
+++ dashboard/App.py
@@ -15,24 +15,24 @@ st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 #     page_icon="👋",
 # )
 
-st.write("# Welcome to Streamlit! 👋")
+st.write("# 全球股市大盘")
 
 # st.sidebar.success("选择市场")
 
-st.markdown(
-    """
-    Streamlit is an open-source app framework built specifically for
-    Machine Learning and Data Science projects.
-    **👈 Select a demo from the sidebar** to see some examples
-    of what Streamlit can do!
-    ### Want to learn more?
-    - Check out [streamlit.io](https://streamlit.io)
-    - Jump into our [documentation](https://docs.streamlit.io)
-    - Ask a question in our [community
-        forums](https://discuss.streamlit.io)
-    ### See more complex demos
-    - Use a neural net to [analyze the Udacity Self-driving Car Image
-        Dataset](https://github.com/streamlit/demo-self-driving)
-    - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
-"""
-)
+# st.markdown(
+#     """
+#     Streamlit is an open-source app framework built specifically for
+#     Machine Learning and Data Science projects.
+#     **👈 Select a demo from the sidebar** to see some examples
+#     of what Streamlit can do!
+#     ### Want to learn more?
+#     - Check out [streamlit.io](https://streamlit.io)
+#     - Jump into our [documentation](https://docs.streamlit.io)
+#     - Ask a question in our [community
+#         forums](https://discuss.streamlit.io)
+#     ### See more complex demos
+#     - Use a neural net to [analyze the Udacity Self-driving Car Image
+#         Dataset](https://github.com/streamlit/demo-self-driving)
+#     - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
+# """
+# )
--- dashboard/pages/2_📈_A股.py
+++ dashboard/pages/2_📈_A股.py
@@ -55,4 +55,37 @@ fig.update_traces(textposition='middle center',
     #     "Col2: %{customdata[1]}",
     #     "Col3: %{customdata[2]}",
     # ])
-st.plotly_chart(fig, use_container_width=True)
+# st.plotly_chart(fig, use_container_width=True)
+fig2 = px.treemap(df, 
+                 path=[px.Constant("全部"),'一级行业','二级行业','三级行业'],  # 指定层次结构，每一个层次都应该是category型的变量
+                 values='流通市值', # 需要聚合的列名
+                 color='涨跌幅', 
+                 custom_data=['涨跌幅','流通市值'],
+                 range_color = [-4, 4], # 色彩范围最大最小值
+                 hover_data= {'涨跌幅':':.2',
+                             '流通市值':':.1f'}, # 鼠标悬浮显示数据的格式
+                 color_continuous_scale=["seagreen",'lightgrey', "indianred"],
+                 height = 500,
+                #  width = 1600,
+                 color_continuous_midpoint=0 , # 颜色变化中间值设置为增长率=0
+                )
+fig2.update_layout({
+'plot_bgcolor': 'rgba(0, 0, 0, 0)',
+'paper_bgcolor': 'rgba(0, 0, 0, 0)',
+})
+fig2.update_coloraxes(showscale=False)
+fig2.update_traces(marker_line_width = 0.5,marker_line_color="white")
+fig2.update_traces(textposition='middle center', 
+                  textinfo='label', 
+                  textfont = dict(color='white'),
+                  texttemplate= "%{label}<br>%{customdata[0]:.2f}%<br>",
+                  hovertemplate= "%{label}<br>涨跌幅=%{customdata[0]:.2f}%<br>流通市值=%{customdata[1]:d}亿") # 显示企业名称和市值，字体24
+
+
+
+
+tab1, tab2 = st.tabs(["板块概览", "个股详情"])
+with tab1:
+    st.plotly_chart(fig2, use_container_width=True, theme = 'streamlit')
+with tab2:
+    st.plotly_chart(fig, use_container_width=True, theme = 'streamlit')
