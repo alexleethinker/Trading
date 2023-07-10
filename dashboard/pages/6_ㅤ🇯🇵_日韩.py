347cb93c865481c63358cdbf805ff721be11e45f
Alex LI <zhi.li@datapebbles.com>
2023-07-10 21:47:57 +0000

    updates

--- dashboard/pages/6_ㅤ🇯🇵_日韩.py
+++ dashboard/pages/6_ㅤ🇯🇵_日韩.py
@@ -31,7 +31,7 @@ df = df.merge(trans_df, on = 'industry').merge(market_df, on = 'market')
 def plot_plate():
     dfi = df[df['market'].isin(['japan','korea'])]
     figi = px.treemap(dfi, 
-                    path=[px.Constant("亚太"),'市场','一级行业','二级行业','description'],  # 指定层次结构，每一个层次都应该是category型的变量
+                    path=[px.Constant("日韩"),'市场','一级行业','二级行业','description'],  # 指定层次结构，每一个层次都应该是category型的变量
     #                  path=['plate','','sector','industry',],
                     values='market_cap_USD', # 需要聚合的列名
                     color='change', 
@@ -57,7 +57,7 @@ def plot_plate():
                     textinfo='label', 
                     textfont = dict(color='white'),
                     texttemplate= "%{label}<br>%{customdata[0]:.2f}%<br>",
-                    hovertemplate= "%{label}<br>%{customdata[1]}<br>%{customdata[3]:.1f} (%{customdata[0]:.2f})%<br>总市值=%{customdata[2]:d}亿"                  
+                    hovertemplate= "%{customdata[1]}<br>%{label}<br>%{customdata[3]:.1f} (%{customdata[0]:.2f})%<br>总市值=%{customdata[2]:d}亿"                  
     #                   hovertemplate= "%{customdata[1]}<br>%{label}<br>(%{customdata[0]:.2f}%)<br>总市值=%{customdata[2]:d}亿"
                     ) 
     return figi
