118dd89476eab3d1fba9f16c1103b5539f12a32e
Zhi LI <alexleethinker@gmail.com>
2023-09-22 09:00:00 +0000

    updates

--- investin/Streamlit/🗞️_主页.py
+++ investin/Streamlit/🗞️_主页.py
@@ -4,8 +4,9 @@ from utils.calendars import get_trading_calendars
 from streamlit_autorefresh import st_autorefresh
 page_config()
 
-
-
+st.markdown("<h4 style='text-align: center;'>主要股票交易所开盘时钟(UTC)</h4>", unsafe_allow_html=True)
+# st.divider()
+"---"
 fig_calendar = get_trading_calendars()
 st.plotly_chart(fig_calendar, use_container_width=True)
 
