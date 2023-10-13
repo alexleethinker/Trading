d98e69ad1e5d7c97b9f22d0edba3c52d6b328897
Zhi LI <alexleethinker@gmail.com>
2023-10-13 10:12:05 +0000

    updates

--- investin/Streamlit/Backtest/DataSource.py
+++ investin/Streamlit/Backtest/DataSource.py
@@ -13,7 +13,7 @@ class HDFDataSource(DataSource):
         pybroker.register_columns('换手率')
 
     def _fetch_data(self, symbols, start_date, end_date, _timeframe, _adjust):
-        store = pd.HDFStore(f'{data_dir}/history/stock/china.h5')        
+        store = pd.HDFStore(f'{data_dir}/history/stock/china.h5','r')        
         df = pd.DataFrame()
         for symbol in symbols:
             dfi = store[symbol]
@@ -22,6 +22,7 @@ class HDFDataSource(DataSource):
             df = pd.concat([df, dfi], ignore_index=True)
         df = df.rename(columns= {'日期':'date','开盘':'open','收盘':'close','最高':'high','最低':'low'})
         df['date'] = pd.to_datetime(df['date'])
+        store.close()
         return df[(df['date'] >= start_date) & (df['date'] <= end_date)]
     
     
--- investin/Streamlit/pages/5_ 🕙_Timeline.py
+++ investin/Streamlit/pages/5_ 🕙_Timeline.py
@@ -1,3 +1,42 @@
-from utils.config import page_config
+from utils.config import page_config, data_dir
 page_config()
 import streamlit as st
+import datetime
+import pandas as pd
+from utils.figure import treemap
+import plotly.express as px
+
+d = st.date_input("选择日期", datetime.date(2019, 7, 1))
+
+
+
+def history_spot(date):
+    store = pd.HDFStore(f'{data_dir}/history/stock/china.h5', 'r')   
+    stock_list = [x.replace('/','') for x in store.keys()]     
+    df = pd.DataFrame()
+    for symbol in stock_list:
+        try:
+            dfi = store[symbol]
+            dfi = dfi[dfi.index == date]
+            dfi['证券代码'] = symbol
+            df = pd.concat([df, dfi], ignore_index=True)
+        except:
+            pass
+    store.close()
+    return df[['证券代码','收盘','涨跌幅','成交额']]
+    
+# st.write(d.strftime('%Y-%m-%d %H:%M:%S'))    
+df = history_spot(d.strftime('%Y-%m-%d'))
+data_path = f'{data_dir}/spot/stock_spot_china_a.csv'
+details = pd.read_csv(data_path)[['证券代码','证券名称','一级行业','二级行业','三级行业']]
+df = df.merge(details, how = 'inner', on = '证券代码')
+df = df[df['成交额'] > df['成交额'].quantile(.6) ]
+fig = treemap(      df, 
+                    path=[px.Constant(d.strftime('%Y-%m-%d')),'一级行业','二级行业','三级行业','证券名称'], 
+                    values='成交额', 
+                    color='涨跌幅', 
+                    range_color = 8, 
+                    custom_data= ['涨跌幅','收盘','成交额'],
+                    hovertemplate = "%{label}<br>%{customdata[1]:.2f} %{customdata[0]:.2f}%<br>" + '成交额' + "=%{customdata[2]:d}亿" 
+                )
+st.plotly_chart(fig, use_container_width=True)
