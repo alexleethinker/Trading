6395c9c744e672afb4b3a3d133dccdde05549012
Zhi LI <alexleethinker@gmail.com>
2023-12-22 11:23:45 +0000

    updates

--- investin/Utils/scheduler.py
+++ investin/Utils/scheduler.py
@@ -47,24 +47,24 @@ def start_cronjob():
     # scheduler.add_job(save_data, 'interval', minutes=1, id='load_data')
     scheduler.add_job(update_spot_data_at_trading_a, 'cron', day_of_week='mon-fri',hour='0-7',minute='*',second='30')
     scheduler.add_job(StockSpotChinaA().run, 'cron', day_of_week='mon-fri',hour='7',minute='1')
-    scheduler.add_job(indices_intraday_china(), 'cron', day_of_week='mon-fri',hour='7',minute='1')
+    scheduler.add_job(indices_intraday_china, 'cron', day_of_week='mon-fri',hour='7',minute='1')
 
     scheduler.add_job(update_spot_europe_at_trading, 'cron', day_of_week='mon-fri',hour='7-16',minute='0/1',second='30')
     scheduler.add_job(StockSpotEuronext().run, 'cron', day_of_week='mon-fri',hour='16',minute='36,55')
     scheduler.add_job(StockSpotXetra().run, 'cron', day_of_week='mon-fri',hour='16',minute='36,55')
     scheduler.add_job(StockSpotUK().run, 'cron', day_of_week='mon-fri',hour='16',minute='36,55')
-    scheduler.add_job(indices_intraday_europe(), 'cron', day_of_week='mon-fri',hour='16',minute='36,55')
+    scheduler.add_job(indices_intraday_europe, 'cron', day_of_week='mon-fri',hour='16',minute='36,55')
 
     
     scheduler.add_job(StockSpotHKEX().run, 'cron', day_of_week='mon-fri',hour='0-8',minute='0/1')
-    scheduler.add_job(indices_intraday_hk(), 'cron', day_of_week='mon-fri',hour='0-8',minute='0/1')
+    scheduler.add_job(indices_intraday_hk, 'cron', day_of_week='mon-fri',hour='0-8',minute='0/1')
     scheduler.add_job(StockSpotUS().run, 'cron', day_of_week='mon-fri',hour='14-22',minute='0/1')
-    scheduler.add_job(indices_intraday_US().run, 'cron', day_of_week='mon-fri',hour='14-22',minute='0/1')    
+    scheduler.add_job(indices_intraday_US, 'cron', day_of_week='mon-fri',hour='14-22',minute='0/1')    
     scheduler.add_job(StockSpotTradingView().run, 'cron', day_of_week='mon-fri',hour='0-22',minute='0/1')
-    scheduler.add_job(indices_intraday_apac(), 'cron', day_of_week='mon-fri',hour='1-8',minute='0/1')
-    scheduler.add_job(indices_intraday_india(), 'cron', day_of_week='mon-fri',hour='3-11',minute='0/1')
+    scheduler.add_job(indices_intraday_apac, 'cron', day_of_week='mon-fri',hour='1-8',minute='0/1')
+    scheduler.add_job(indices_intraday_india, 'cron', day_of_week='mon-fri',hour='3-11',minute='0/1')
     
-    scheduler.add_job(indices_intraday(), 'cron', day_of_week='mon-fri',hour='22',minute='1')
+    scheduler.add_job(indices_intraday, 'cron', day_of_week='mon-fri',hour='22',minute='1')
     scheduler.start()
 
 if __name__ == "__main__":
