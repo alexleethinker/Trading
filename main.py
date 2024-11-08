17ea6aca49ab48744362c270291a2485fb2c2944
Alex LI <alexleethinker@gmail.com>
2024-11-09 02:15:34 +0800

    updates

--- main.py
+++ main.py
@@ -7,10 +7,10 @@ from investin.Utils.DataLoader.Euronext import StockSpotEuronext
 from investin.Utils.DataLoader.Xetra import StockSpotXetra
 from investin.Utils.DataLoader.TradingView import StockSpotTradingView
 
-# StockSpotChinaA().run()
+StockSpotChinaA().run()
 # StockSpotUS().run()
 # StockSpotHKEX().run()
-StockSpotTradingView().run()
+# StockSpotTradingView().run()
 # StockSpotEuronext().run()
 # StockSpotXetra().run()
 
